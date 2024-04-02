5cb51a1cd58856019f36ad77048538bffaa98ce4
ederg <ederg@DESKTOP-PBKGKQT>
2024-04-02 15:58:56 +0200

    Inicio

---
+++ proEderBorja/src/proEderBorja/HolaMundo.java
@@ -0,0 +1,13 @@
+package proEderBorja;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Hola Mundo!!!");
+		
+	}
+
+}
