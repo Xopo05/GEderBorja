2c299c2d789396a1a431a12dfa41ed9f372b4c96
ederg <ederg@DESKTOP-PBKGKQT>
2024-04-04 10:42:16 +0200

    Sexto cambio segundo usuario, sin conflictos

--- proEderBorja/src/proEderBorja/HolaMundo.java
+++ proEderBorja/src/proEderBorja/HolaMundo.java
@@ -1,12 +1,14 @@
 package proEderBorja;
 
 public class HolaMundo {
-
+	
 	private int restar (int a, int b ) {
 		return a - b;
 	}
 	
+
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 
 		
 		System.out.println("Hola Mundo!!!");
