81397a5a301319d562ab370f43348b871fcac220
ederg <ederg@DESKTOP-PBKGKQT>
2024-04-04 10:31:22 +0200

    Cuarto cambio segundo usuario

--- proEderBorja/src/proEderBorja/HolaMundo.java
+++ proEderBorja/src/proEderBorja/HolaMundo.java
@@ -3,14 +3,13 @@ package proEderBorja;
 public class HolaMundo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 		
 		System.out.println("Hola Mundo!!!");
 		System.out.print("Hola otra vez ");
 		System.out.print("HOLA GITHUB");
 		
-		System.out.print("cuarto cambio");
+		System.out.print("cuarto cambio primer usuario");
 	}
 
 }
