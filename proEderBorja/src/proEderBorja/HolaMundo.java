dd7e3e742b78e9cd2560e90ab4913c07fc2bc44b
ederg <ederg@DESKTOP-PBKGKQT>
2024-04-04 10:37:02 +0200

    quinto cambio primer usuario

--- proEderBorja/src/proEderBorja/HolaMundo.java
+++ proEderBorja/src/proEderBorja/HolaMundo.java
@@ -1,6 +1,11 @@
 package proEderBorja;
 
 public class HolaMundo {
+	
+	private int sumar (int a, int b ) {
+		return a + b;
+	}
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
